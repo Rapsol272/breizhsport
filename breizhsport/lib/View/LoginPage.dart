de44e8adba258b876064d88550bb1188d19ec700
tpeyr <tpeyrot71@gmail.com>
2024-02-06 00:09:36 +0100

    Started to fix login page

--- breizhsport/lib/View/LoginPage.dart
+++ breizhsport/lib/View/LoginPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../Controllers/Auth.dart';
 import 'SignInPage.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -16,56 +15,68 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              child: Text('Sign In'),
-              onPressed: () async {
-                final email = _emailController.text;
-                final password = _passwordController.text;
-                final user = await _auth.signInWithEmailAndPassword(email, password);
-                if (user != null) {
-                  Navigator.pop(context);
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Invalid email or password')),
-                  );
-                }
-              },
-            ),
-            SizedBox(height: 16),
-            TextButton(
-              child: Text('Create Account'),
-              onPressed: () 
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignInPage()),
-                );
-              },
-            ),
-          ],
+      appBar: AppBar(),
+      // body: Card(
+      //     child: Padding(
+      //         padding: EdgeInsets.all(32),
+      //         child: Column(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             TextField(
+      //               controller: _emailController,
+      //               decoration: InputDecoration(
+      //                 labelText: 'Email',
+      //               ),
+      //             ),
+      //             SizedBox(height: 16),
+      //             TextField(
+      //               controller: _passwordController,
+      //               decoration: InputDecoration(
+      //                 labelText: 'Password',
+      //               ),
+      //               obscureText: true,
+      //             ),
+      //             SizedBox(height: 16),
+      //             ElevatedButton(
+      //               child: Text('Sign In'),
+      //               onPressed: () async {
+      //                 final email = _emailController.text;
+      //                 final password = _passwordController.text;
+      //                 final user = await _auth.signInWithEmailAndPassword(
+      //                     email, password);
+      //                 if (user != null) {
+      //                   Navigator.pop(context);
+      //                 } else {
+      //                   ScaffoldMessenger.of(context).showSnackBar(
+      //                     SnackBar(
+      //                         content: Text('Invalid email or password')),
+      //                   );
+      //                 }
+      //               },
+      //             ),
+      //             SizedBox(height: 16),
+      //             TextButton(
+      //               child: Text('Create Account'),
+      //               onPressed: () {
+      //                 Navigator.push(
+      //                   context,
+      //                   MaterialPageRoute(builder: (context) => SignInPage()),
+      //                 );
+      //               },
+      //             ),
+      //           ],
+      //         )))
+      body: Container(
+        alignment: Alignment.center,
+        child: const Card(
+          child: Padding(
+              padding: EdgeInsets.all(32),
+              child: Column(children: [
+                Text(
+                  "Se connecter",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ])),
         ),
       ),
     );
