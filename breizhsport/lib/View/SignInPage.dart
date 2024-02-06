0fd581c8dff4cb2e0d6e53cea5ca5a5439dc80f1
tpeyr <tpeyrot71@gmail.com>
2024-02-06 18:25:35 +0100

    Finished basic layout for signin page

--- breizhsport/lib/View/SignInPage.dart
+++ breizhsport/lib/View/SignInPage.dart
@@ -1,4 +1,5 @@
 import 'package:breizhsport/Controllers/Auth.dart';
+import 'package:breizhsport/View/LoginPage.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -16,80 +17,200 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Sign In'),
+    //   ),
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         const Text(
+    //           'Welcome to the Sign In Page!',
+    //           style: TextStyle(fontSize: 16),
+    //         ),
+    //         const SizedBox(height: 16),
+    //         TextFormField(
+    //           controller: _emailController,
+    //           decoration: const InputDecoration(
+    //             labelText: 'Email',
+    //           ),
+    //         ),
+    //         const SizedBox(height: 8),
+    //         TextFormField(
+    //           controller: _passwordController,
+    //           decoration: const InputDecoration(
+    //             labelText: 'Password',
+    //           ),
+    //           obscureText: true,
+    //         ),
+    //         //ajout demande de tel , nom , prenom
+    //         const SizedBox(height: 8),
+    //         TextFormField(
+    //           controller: _phoneController,
+    //           decoration: const InputDecoration(
+    //             labelText: 'Numéro de téléphone',
+    //           ),
+    //         ),
+    //         const SizedBox(height: 8),
+    //         TextFormField(
+    //           controller: _firstNameController,
+    //           decoration: const InputDecoration(
+    //             labelText: 'First Name',
+    //           ),
+    //         ),
+    //         const SizedBox(height: 8),
+    //         TextFormField(
+    //           controller: _lastNameController,
+    //           decoration: const InputDecoration(
+    //             labelText: 'Last Name',
+    //           ),
+    //         ),
+    //         const SizedBox(height: 16),
+    //         ElevatedButton(
+    //           onPressed: () async {
+    //             final email = _emailController.text;
+    //             final password = _passwordController.text;
+    //             final user = await _auth.registerWithEmailAndPassword(
+    //                 _firstNameController.text,
+    //                 _lastNameController.text,
+    //                 _phoneController.text,
+    //                 email,
+    //                 password);
+    //             //si ok redirection vers la page d'accueil sinon message d'erreur
+    //             if (user != null) {
+    //               Navigator.pop(context);
+    //             } else {
+    //               ScaffoldMessenger.of(context).showSnackBar(
+    //                 SnackBar(content: Text('Erreur de creation de compte')),
+    //               );
+    //             }
+    //           },
+    //           child: Text('Sign In'),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to the Sign In Page!',
-              style: TextStyle(fontSize: 16),
+        appBar: AppBar(),
+        body: Center(
+          child: Container(
+            // Add padding to center the child in the middle of the screen
+            padding: const EdgeInsets.all(32),
+            constraints: const BoxConstraints(
+              maxWidth: 640,
             ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
+            child: Card(
+              // Add a border to the card
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  side: BorderSide(color: Colors.grey)),
+              child: Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Column(children: [
+                    const Text(
+                      "Créer un compte",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 32),
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                        ),
+                        labelText: 'Adresse mail',
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    // Row of text fields
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _firstNameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 1),
+                              ),
+                              labelText: 'Prénom',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 32),
+                        Expanded(
+                          child: TextField(
+                            controller: _lastNameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 1),
+                              ),
+                              labelText: 'Nom',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 32),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                        ),
+                        labelText: 'Mot de passe',
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final email = _emailController.text;
+                        final password = _passwordController.text;
+                        final user = await _auth.registerWithEmailAndPassword(
+                            _firstNameController.text,
+                            _lastNameController.text,
+                            _phoneController.text,
+                            email,
+                            password);
+                        //si ok redirection vers la page d'accueil sinon message d'erreur
+                        if (user != null) {
+                          Navigator.pop(context);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text('Erreur de creation de compte')),
+                          );
+                        }
+                      },
+                      child: const Text('Créer le compte'),
+                    ),
+                    const SizedBox(height: 32),
+                    const Divider(),
+                    const SizedBox(height: 32),
+                    // row with text buttons to create an account
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Déjà un compte? "),
+                        const SizedBox(width: 32),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()),
+                            );
+                          },
+                          child: const Text('Se connecter'),
+                        ),
+                      ],
+                    ),
+                  ])),
             ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            //ajout demande de tel , nom , prenom
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _phoneController,
-              decoration: const InputDecoration(
-                labelText: 'Numéro de téléphone',
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _firstNameController,
-              decoration: const InputDecoration(
-                labelText: 'First Name',
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _lastNameController,
-              decoration: const InputDecoration(
-                labelText: 'Last Name',
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                final email = _emailController.text;
-                final password = _passwordController.text;
-                final user = await _auth.registerWithEmailAndPassword(
-                    _firstNameController.text,
-                    _lastNameController.text,
-                    _phoneController.text,
-                    email,
-                    password);
-                //si ok redirection vers la page d'accueil sinon message d'erreur
-                if (user != null) {
-                  Navigator.pop(context);
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Erreur de creation de compte')),
-                  );
-                }
-              },
-              child: Text('Sign In'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
