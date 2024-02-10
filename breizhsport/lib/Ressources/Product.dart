11133bd547cb80fad5e1a4cd3f4f373289be7a1e
tpeyr <tpeyrot71@gmail.com>
2024-02-10 18:17:06 +0100

    Test

--- breizhsport/lib/Ressources/Product.dart
+++ breizhsport/lib/Ressources/Product.dart
@@ -100,7 +100,7 @@ class ProductWidget extends StatelessWidget {
                               ))),
                       //ajout de la description du produit limmite a 4 lignes
                       Text(product.description,
-                          maxLines: 3,
+                          maxLines: 2,
                           style: TextStyle(
                             fontSize: (constraints.maxHeight * 0.04),
                             fontWeight: FontWeight.w100,
